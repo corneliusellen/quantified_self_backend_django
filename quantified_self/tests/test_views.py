6a54c01cd093d1ad1597edd5f76a69e68bbb8a2a
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-12 11:49:56 -0600

    Write test for get all foods.

--- quantified_self/tests/test_views.py
+++ quantified_self/tests/test_views.py
@@ -6,3 +6,19 @@ from ..models import Food
 from ..serializers import FoodSerializer
 
 client = Client()
+
+class GetAllFoodsTest(TestCase):
+    def setUp(self):
+        Food.objects.create(
+            name='Pudding',
+            calories=500)
+        Food.objects.create(
+            name='Radishes',
+            calories=150)
+
+    def test_get_all_foods(self):
+        response = client.get(reverse('get_post_food'))
+        foods = Food.objects.all()
+        serializer = FoodSerializer(foods, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
