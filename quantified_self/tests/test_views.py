05c4d595083b1a744899a4cce61b918d0be65896
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-12 13:44:31 -0600

    Create post food test. Fix preious get one food test.

--- quantified_self/tests/test_views.py
+++ quantified_self/tests/test_views.py
@@ -34,12 +34,41 @@ class GetOneFoodTest(TestCase):
             calories=150)
 
     def test_get_one_food_valid(self):
-        response = client.get(reverse('get_delete_update_food', kwargs={'pk': self.radishes.pk}))
+        response = client.get(reverse('get_delete_update_food', kwargs={'pk': self.pudding.pk}))
         food = Food.objects.get(pk=self.pudding.pk)
         serializer = FoodSerializer(food)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_one_food_invalid(self):
-        response = client.get(reverse('get_delete_update_food', kwargs={'pk': 5}))
+        response = client.get(reverse('get_delete_update_food', kwargs={'pk': 3005}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+class CreateNewFoodTest(TestCase):
+
+    def setUp(self):
+        self.valid_payload = {
+            'name': 'Muffin',
+            'calories': 400
+        }
+        self.invalid_payload = {
+            'name': '',
+            'calories': 2
+        }
+
+    def test_create_food_valid(self):
+        response = client.post(
+            reverse('get_post_food'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_food_invalid(self):
+        response = client.post(
+            reverse('get_post_food'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
