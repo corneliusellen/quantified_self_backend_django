d80facba43c3b44d1d33e18fbabd508ac49e37ec
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-12 11:34:55 -0600

    Create shell of view for foodCRUD.

--- quantified_self/views.py
+++ quantified_self/views.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
+# from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Food
+from .serializers import FoodSerializer
 
-from django.shortcuts import render
+@api_view(['GET', 'DELETE', 'PATCH'])
+def get_delete_update_food(request, pk):
+    try:
+        food = Food.objects.get(pk=pk)
+    except Food.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-# Create your views here.
+    if request.method == 'GET':
+        return Response({})
+    elif request.method == 'DELETE':
+        return Response({})
+    elif rquest.method == 'PUT':
+        return Response({})
+
+@api_view(['GET', 'POST'])
+def get_post_food(request):
+    if request.method == 'GET':
+        return Response({})
+    elif request.method == 'POST':
+        return Response({})
