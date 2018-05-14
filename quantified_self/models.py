bc115d90c82448e2ec8e78033c1c32755d833518
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-14 14:52:05 -0600

    Run meal migration.

--- quantified_self/models.py
+++ quantified_self/models.py
@@ -10,3 +10,8 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
+
+class Meal(models.Model):
+    name = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
