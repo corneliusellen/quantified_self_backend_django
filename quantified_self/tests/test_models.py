a2af3f677083b87147290e2a382957e41df67742
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-14 16:37:15 -0600

    Create model test for meal and meal food. Passing.

--- quantified_self/tests/test_models.py
+++ quantified_self/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from ..models import Food
+from ..models import Meal
 
 class FoodTest(TestCase):
 
@@ -15,6 +16,33 @@ class FoodTest(TestCase):
         marshmellow = Food.objects.get(name='Marshmellow')
         escargot = Food.objects.get(name='Escargot')
         self.assertEqual(
-            marshmellow.__str__(), "Marshmellow")
+            marshmellow.__str__(), 'Marshmellow')
         self.assertEqual(
-            escargot.name, "Escargot")
+            escargot.name, 'Escargot')
+
+class MealTest(TestCase):
+
+    def setUp(self):
+        Meal.objects.create(
+            name='Dinner')
+
+    def test_meal_name(self):
+        dinner = Meal.objects.get(name='Dinner')
+        self.assertEqual(
+            dinner.name, 'Dinner')
+
+class MealFoods(TestCase):
+
+    def setUp(self):
+        marshmellow = Food.objects.create(
+            name='Marshmellow',
+            calories=200)
+        dinner = Meal.objects.create(
+            name='Dinner')
+        dinner.foods.add(marshmellow)
+
+    def test_mealfoods_name(self):
+        meal = Meal.objects.get(name='Dinner')
+        food = Food.objects.get(name='Marshmellow')
+        self.assertEqual(
+            meal.foods.all()[:1].get(), food)
