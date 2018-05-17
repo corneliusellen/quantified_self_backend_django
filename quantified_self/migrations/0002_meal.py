29539f732dcf57b7dfec0ce027404d82a355891f
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-17 16:01:49 -0600

    Run migrations for updated model.

--- quantified_self/migrations/0002_meal.py
+++ quantified_self/migrations/0002_meal.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.utils import timezone
 
 
 class Migration(migrations.Migration):
@@ -17,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=300)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True, default=timezone.now)),
             ],
         ),
     ]
