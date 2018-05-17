1cf0b5af04dfb65e7cf8d00385619579b9cca510
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-17 17:38:14 -0600

    Add database host back in.

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'quantified_self_python_development',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
