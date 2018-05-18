4cd8a4b1132c895b506adddd444bcdeafd3a24fb
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-17 23:05:37 -0600

    Add meal serializer.

--- quantified_self/serializers.py
+++ quantified_self/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Food
+from .models import Meal
 
 class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Food
         fields = ('id', 'name', 'calories')
+
+class MealSerializer(serializers.ModelSerializer):
+    foods = FoodSerializer(many=True)
+
+    class Meta:
+        model = Meal
+        fields = ('id', 'name', 'foods')
